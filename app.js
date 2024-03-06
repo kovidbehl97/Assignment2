5bb0bbc179855946ee0dedbb718f1a9e1e2e0782
kovidbehl97 <kovid.behl@gmail.com>
2024-03-05 21:36:50 -0500

    updated code

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const cors = require("cors");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const fs = require("fs");
